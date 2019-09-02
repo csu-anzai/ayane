a857ac5cc4c269c77d1c327bbb290f3c0c0c9206
Russell Wallace <russell.wallace@gmail.com>
2019-09-02 12:14:09 +0100

    reproducible build

--- release.bat
+++ release.bat
@@ -1,3 +1,3 @@
 if "%VCINSTALLDIR%"=="" call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall" x64
 move *.asm \tmp
-cl /Fa /Feayane /I\mpir /O2 *.cc \mpir\lib\x64\Release\mpir.lib
+cl /Fa /Feayane /I\mpir /O2 *.cc \mpir\lib\x64\Release\mpir.lib /link /Brepro
